32e5f603e61ae7b796c99699a536a71a0913f089
dmitriy-schuka <dmitriy.schuka@devit.group>
2024-01-18 15:08:25 +0200

    feat: init sequelize

---
+++ src/settings/db/postgres.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    migrationStorage: "json",
+    seederStorage: "json",
+  },
+  test: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    migrationStorage: "json",
+    seederStorage: "json",
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    migrationStorage: "json",
+    seederStorage: "json",
+  },
+}
