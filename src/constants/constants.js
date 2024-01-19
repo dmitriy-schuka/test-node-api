f7b59ed4ac3dfb2c0ca2914c71b094dc5776c7ee
dmitriy-schuka <dmitriy.schuka@devit.group>
2024-01-19 13:53:01 +0200

    feat: create new constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,6 @@
 module.exports = {
-  USER_ROLES: ['user', 'admin'],
+  USER_ROLES: ['admin', 'user'],
+  SALT_ROUNDS: 5,
+  ACCESS_TOKEN_TIME: 60 * 60 * 24,
+  JWT_SECRET: process.env.JWT_SECRET
 }
