574092f98b367688d4b8e1212552d5fd8b289d5b
dmitriy-schuka <dmitriy.schuka@devit.group>
2024-01-19 13:53:26 +0200

    feat: create swagger docs

--- src/settings/swagger/swaggerRoutes.js
+++ src/settings/swagger/swaggerRoutes.js
@@ -3,6 +3,8 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./api-docs.json');
 
 const swaggerRoutes = express.Router();
-swaggerRoutes.get('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+swaggerRoutes.use('/', swaggerUi.serve);
+swaggerRoutes.get('/', swaggerUi.setup(swaggerDocument, { swaggerOptions: { url: '/' } }));
 
 module.exports = swaggerRoutes;
